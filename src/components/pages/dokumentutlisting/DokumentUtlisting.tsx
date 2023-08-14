90902d5daf92952e2135d4f8c02034124ef736b5
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2023-08-14 09:42:04 +0200

    WIP - test av sjekk for ingen dokumenter

--- src/components/pages/dokumentutlisting/DokumentUtlisting.tsx
+++ src/components/pages/dokumentutlisting/DokumentUtlisting.tsx
@@ -39,15 +39,15 @@ const DokumentUtlisting = () => {
   }
 
   const isContent = dokumentliste?.length > 0;
-  const temaNavn = dokumentliste && dokumentliste[0].navn;
-  const dato = dokumentliste && format(new Date(dokumentliste[0].journalposter[0].sisteEndret), "dd.MM.yyyy");
+  const temaNavn = dokumentliste && dokumentliste[0]?.navn;
+  const dato = dokumentliste && format(new Date(dokumentliste[0]?.journalposter[0].sisteEndret), "dd.MM.yyyy");
 
   return (
     <>
       {isContent ? (
         <div>
           <Heading level="2" size="xlarge">
-            {dokumentliste ? dokumentliste[0].navn : text.dokumentArkivTittel[language]}
+            {dokumentliste ? dokumentliste[0]?.navn : text.dokumentArkivTittel[language]}
           </Heading>
           <BodyShort className={styles.sistEndret}>{text.sistEndret[language] + " " + dato}</BodyShort>
           <Ingress className={styles.ingress}>{text.dokumentArkivIngress[language] + " " + temaNavn}</Ingress>
