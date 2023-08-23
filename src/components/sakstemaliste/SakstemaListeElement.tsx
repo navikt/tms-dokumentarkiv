6200ffcbdb607e146fedad344100881497a07642
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2023-08-23 09:28:19 +0200

    Bytter om fra a-tag til Link for å bevare state innad i app ved bytte av side

--- src/components/sakstemaliste/SakstemaListeElement.tsx
+++ src/components/sakstemaliste/SakstemaListeElement.tsx
@@ -6,20 +6,23 @@ import { text } from "../../language/text";
 import { languageAtom } from "../../store/store";
 import { SakstemaElement } from "./SakstemaListe";
 import styles from "./SakstemaListeElement.module.css";
+import { Link } from "react-router-dom";
 
 const SakstemaListeElement = ({sakstema} : {sakstema: SakstemaElement}) => {
 
   const language = useStore(languageAtom);
 
+  const url = `/dokumentarkiv/tema/${sakstema.kode}`
+
   return(
     <li className={styles.container} key={Math.random.toString()}>
-      <a href={sakstema.detaljvisningUrl} className={styles.lenke}>
+      <Link to={url} className={styles.lenke}>
         <FolderFillIcon fontSize="1.75rem"/>
         <div>
         <BodyShort className={styles.tittel}>{sakstema.navn}</BodyShort>
         <BodyLong className={styles.ingress}>{`${text.sistEndret[language]} ${format(new Date(sakstema.sistEndret), "dd.MM.yyyy")}`}</BodyLong>
         </div>
-      </a>
+      </Link>
     </li>
   );
 };
