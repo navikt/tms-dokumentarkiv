0bdddc47c708363e94658cdb0faae98e42a3307a
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2023-09-15 14:55:30 +0200

    Fjerner useBatch: true fra amplitude

--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -2,7 +2,6 @@ import { init, track } from "@amplitude/analytics-browser";
 
 export const initAmplitude = () => {
   init("default", undefined, {
-    useBatch: true,
     serverUrl: "https://amplitude.nav.no/collect-auto",
     ingestionMetadata: {
       sourceName: window.location.toString(),
