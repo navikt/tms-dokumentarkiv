3a60d8648e3a949d2531a78f988e7a5129d0b281
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2023-09-18 11:22:22 +0200

    Legger med saveEvents: false igjen, siden true ikke førte til noen endring og false er i tråd med aksel dokumentasjon

--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -4,6 +4,7 @@ import amplitude from "amplitude-js";
 export const initializeAmplitude = () => {
   amplitude.getInstance().init("default", "", {
     apiEndpoint: "amplitude.nav.no/collect-auto",
+    saveEvents: false,
     includeUtm: true,
     includeReferrer: true,
     platform: window.location.toString(),
